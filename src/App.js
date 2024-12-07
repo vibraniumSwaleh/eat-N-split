62ee10781df16e8e0747353524cb72e6bb683c17
vibraniumSwaleh <hus.swaleh@yahoo.com>
2024-12-07 22:50:35 +0000

    render friend list

--- src/App.js
+++ src/App.js
@@ -42,7 +42,28 @@ function FriendList() {
 }
 
 function Friend({ friend }) {
-  return <li>{friend.name}</li>;
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className='red'>
+          You owe {friend.name} {Math.abs(friend.balance)}€
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className='green'>
+          You owe {friend.name} {Math.abs(friend.balance)}€
+        </p>
+      )}
+      {friend.balance === 0 && (
+        <p>
+          You owe {friend.name} {Math.abs(friend.balance)}€
+        </p>
+      )}
+    </li>
+  );
 }
 
 export default App;
